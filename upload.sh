6257296cb3e55880aa6acde667c752333c000238
Javier Ubillos <javier@ubillos.org>
2017-01-09 09:50:56 +0100

    Added exclude dirs to upload.sh (and command-line specifiable destination.

--- upload.sh
+++ upload.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 REMOTEUSER=pi
 REMOTEHOST=192.168.0.26
+REMOTECONN=${1:-$REMOTEUSER@$REMOTEHOST}
+
+EXCLUDE="dlib*/build/ tools/python/build/"
+EXCLUDEMERGEDARGS=$(for PATH in $EXCLUDE; do echo "--exclude $PATH"; done)
 
 #Assumes we have uploaded our ssh-pubkey.
 # cat ~/.ssh/id_rsa.pub | ssh user@hostname 'cat >> .ssh/authorized_keys'
 
-rsync -avz . $REMOTEUSER@$REMOTEHOST:$(basename $(pwd))
+rsync -avz $EXCLUDEMERGEDARGS . $REMOTECONN:$(basename $(pwd))
 
